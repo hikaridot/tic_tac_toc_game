28a528a619305c21d4c00fc409a066009e938b82
hikaridot <hamza.elmakiki@gmail.com>
2025-01-16 22:50:43 +0100

    Updated freeSpaces code

--- tictactoc.c
+++ tictactoc.c
@@ -52,5 +52,13 @@ void printBoard()
 }   
 int checkFreeSpaces()
 {
-
+   int freeSpaces = 9;
+   
+   for (int i = 0; i < 3; i++)
+   {
+      for (int j = 0; j < 3; j++)
+      {
+         
+      }
+   }
 }
